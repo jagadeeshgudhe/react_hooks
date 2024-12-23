87a0fd5317f07909c1dbff1c5fac81d8585bad06
Gudhe Jagadeesh <jagadeesh8808@gmail.com>
2024-12-24 00:01:21 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Timer from './useeffect';
+import FavoriteCar from './usestate';
 
 function App() {
   return (
     <div>
       <Timer />
+      <FavoriteCar />
     </div>
   );
 }
