0e3c2519ee3b0cbab699a1e5570769a04cafdeef
Gudhe Jagadeesh <jagadeesh8808@gmail.com>
2024-12-27 23:58:27 +0530

    updated

--- src/usestate.jsx
+++ src/usestate.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-function FavoriteCar() {
-  const [car, setCar] = useState("Tesla");
+// function FavoriteCar() {
+//   const [car, setCar] = useState("Tesla");
 
-  return (
-    <>
-      <h1>My favorite car is {car}!</h1>
-      <button type="button" onClick={() => setCar("Tesla")}>Tesla</button>
-      <button type="button" onClick={() => setCar("BMW")}>BMW</button>
-      <button type="button" onClick={() => setCar("Audi")}>Audi</button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h1>My favorite car is {car}!</h1>
+//       <button type="button" onClick={() => setCar("Tesla")}>Tesla</button>
+//       <button type="button" onClick={() => setCar("BMW")}>BMW</button>
+//       <button type="button" onClick={() => setCar("Audi")}>Audi</button>
+//     </>
+//   );
+// }
 
-export default FavoriteCar;
+// export default FavoriteCar;
